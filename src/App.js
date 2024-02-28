01277d1f24382eed772ca64d6f3e6115ed7a4a79
SALIH A <100024759+salihslx@users.noreply.github.com>
2024-02-28 12:57:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import image from './Assets/images/ieeecekallooppara.png';
 
 
 function App() {
@@ -246,7 +245,7 @@ function App() {
       <h1>Tickets Now: </h1>
       
       <div className="h2-link">Go to Registration Link :<t/> <a className="link-color">click here</a> </div>
-      <img className="upi-image" src={image} alt="For Payment ->" />
+      <img className="upi-image" src="https://i.postimg.cc/wByrZ6nX/ieeecekallooppara-sbi-pdf.png" alt="For Payment ->" />
     </section>
 
     {/* Footer section */}
